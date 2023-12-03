6f36b4134ba22b90e48f29f33494133662005462
astelmach01 <astelmach20@gmail.com>
2023-12-03 10:16:57 -0800

    Refactor popup.js to use async/await for asynchronous operations

--- popup.js
+++ popup.js
@@ -1,53 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add the event listener to the button
-  document
-    .getElementById("summarizeButton")
-    .addEventListener("click", summarizeCurrentPage);
+document.addEventListener("DOMContentLoaded", async function () {
+  const summarizeButton = document.getElementById("summarizeButton");
+  summarizeButton.addEventListener("click", summarizeCurrentPage);
 
-  // Also run the summarize function immediately when the popup loads
-  summarizeCurrentPage();
+  // Run the summarize function immediately when the popup loads
+  await summarizeCurrentPage();
 });
 
-function summarizeCurrentPage() {
-  getSessionId().then((sessionId) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          function: summarizePageContent,
-        },
-        (injectionResults) => {
-          for (const frameResult of injectionResults) {
-            sendTextToServerForSummarization(frameResult.result, sessionId);
-          }
-        }
-      );
+async function summarizeCurrentPage() {
+  try {
+    const sessionId = await getSessionId();
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    const injectionResults = await chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: summarizePageContent,
     });
-  });
+
+    for (const frameResult of injectionResults) {
+      await sendTextToServerForSummarization(frameResult.result, sessionId);
+    }
+  } catch (error) {
+    console.error("Error in summarizeCurrentPage:", error);
+  }
 }
 
-function sendTextToServerForSummarization(text, sessionId) {
-  fetch("http://localhost:3000/api/summarize", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ content: text, sessionId: sessionId }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("summary").innerHTML = `<p>${data.summary}</p>`;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+async function sendTextToServerForSummarization(text, sessionId) {
+  try {
+    const response = await fetch("http://localhost:3000/api/summarize", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content: text, sessionId }),
     });
+
+    const data = await response.json();
+    document.getElementById("summary").innerHTML = `<p>${data.summary}</p>`;
+  } catch (error) {
+    console.error("Error sending text to server for summarization:", error);
+  }
 }
 
 function summarizePageContent() {
-  return document.body.innerText; // This will be the result passed to the injectionResults
+  return document.body.innerText;
 }
 
-function getSessionId() {
+async function getSessionId() {
   return new Promise((resolve, reject) => {
     chrome.storage.local.get(["sessionId"], function (result) {
       if (result.sessionId) {
